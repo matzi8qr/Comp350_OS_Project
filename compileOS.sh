21c1fcbb59c36a6bc1253ab25d6cbd8c5e5142da
matzi8qr <tyler.wormald@gmail.com>
2023-03-16 23:26:22 -0400

    fix compileOS.sh

--- compileOS.sh
+++ compileOS.sh
@@ -1,3 +1,5 @@
+# Compiles OS onto image diskc.img
+
 dd if=/dev/zero of=diskc.img bs=512 count=256
 nasm bootload.asm
 dd if=bootload of=diskc.img conv=notrunc
@@ -9,6 +11,13 @@ ld86 -o kernel -d kernel_c.o kernel_asm.o
 bcc -ansi -c -o shell_c.o shell.c
 as86 userlib.asm -o userlib_asm.o
 ld86 -o shell -d shell_c.o userlib_asm.o
+
+gcc -o loadFile loadFile.c
+
+bcc -ansi -c -o letter.o letter.c
+ld86 -o letter -d letter.o userlib_asm.o
+bcc -ansi -c -o number.o number.c
+ld86 -o number -d number.o number.c
 
 ./loadFile kernel
 ./loadFile shell
